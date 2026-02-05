b82e07df8e15c281799a49d98db0315110b21d82
Jerome631 <jeromedevassy01@gmail.com>
2026-02-05 00:45:27 +0000

    Lombok cleanup

--- apps/backend/src/main/java/com/tus2026/backend/Models/Member.java
+++ apps/backend/src/main/java/com/tus2026/backend/Models/Member.java
@@ -1,9 +1,15 @@
 package com.tus2026.backend.Models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.List;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Document(collection = "members")
 public class Member {
 
@@ -15,72 +21,4 @@ public class Member {
     private String personalBackground;
     private List<String> skills;
     private int availableCapacity;
-
-    public Member(String id, String name, String position,
-                    String personalBackground, List<String> skills,
-                    int availableCapacity) {
-        this.id = id;
-        this.name = name;
-        this.position = position;
-        this.personalBackground = personalBackground;
-        this.skills = skills;
-        this.availableCapacity = availableCapacity;
-    }
-
-
-    //For logs and debugging
-    @Override
-    public String toString(){
-        return String.format(
-            "Member[id=%s, name:'%s', position:'%s', personal_background:'%s, skills:%s, available_capacity:%s]",
-            id, name, position, personalBackground, String.join(",",skills), availableCapacity);
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPosition() {
-        return position;
-    }
-
-    public void setPosition(String position) {
-        this.position = position;
-    }
-
-    public String getPersonalBackground() {
-        return personalBackground;
-    }
-
-    public void setPersonalBackground(String personalBackground) {
-        this.personalBackground = personalBackground;
-    }
-
-    public List<String> getSkills() {
-        return skills;
-    }
-
-    public void setSkills(List<String> skills) {
-        this.skills = skills;
-    }
-
-    public int getAvailableCapacity() {
-        return availableCapacity;
-    }
-
-    public void setAvailableCapacity(int availableCapacity) {
-        this.availableCapacity = availableCapacity;
-    }
 }
--- apps/backend/src/main/java/com/tus2026/backend/Models/Task.java
+++ apps/backend/src/main/java/com/tus2026/backend/Models/Task.java
@@ -1,82 +1,22 @@
 package com.tus2026.backend.Models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Document(collection = "tasks")
 public class Task {
 
-    @Id
+   @Id
     private String id;
-
     private String taskName;
     private Integer difficulty;
     private String taskDesc;
     private String assigneeId; // reference to Employee id
     private int effort;
-
-    public Task(String taskName, Integer difficulty, String taskDesc,
-                String assigneeId, int effort) {
-        this.taskName = taskName;
-        this.difficulty = difficulty;
-        this.taskDesc = taskDesc;
-        this.assigneeId = assigneeId;
-        this.effort = effort;
-    }
-
-    //For logs and debugging
-    @Override
-    public String toString(){
-        return String.format(
-            "Task[task_name=%s, difficulty:'%s', task_desc:'%s', assignee_id:'%s, effort:%s]",
-            id, taskName, difficulty, taskDesc, assigneeId, effort);
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getTaskName() {
-        return taskName;
-    }
-
-    public void setTaskName(String taskName) {
-        this.taskName = taskName;
-    }
-
-    public Integer getDifficulty() {
-        return difficulty;
-    }
-
-    public void setDifficulty(Integer difficulty) {
-        this.difficulty = difficulty;
-    }
-
-    public String getTaskDesc() {
-        return taskDesc;
-    }
-
-    public void setTaskDesc(String taskDesc) {
-        this.taskDesc = taskDesc;
-    }
-
-    public String getAssigneeId() {
-        return assigneeId;
-    }
-
-    public void setAssigneeId(String assigneeId) {
-        this.assigneeId = assigneeId;
-    }
-
-    public int getEffort() {
-        return effort;
-    }
-
-    public void setEffort(int effort) {
-        this.effort = effort;
-    }
 }
