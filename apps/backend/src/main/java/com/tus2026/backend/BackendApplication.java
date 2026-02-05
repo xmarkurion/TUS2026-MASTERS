82519ab5932f32bbf97f5c7ead726e3f49b19375
Jerome631 <jeromedevassy01@gmail.com>
2026-02-05 00:46:12 +0000

    DB test setup

--- apps/backend/src/main/java/com/tus2026/backend/BackendApplication.java
+++ apps/backend/src/main/java/com/tus2026/backend/BackendApplication.java
@@ -1,13 +1,23 @@
 package com.tus2026.backend;
 
+import com.tus2026.backend.Repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class BackendApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
 
+    @Autowired
+    private TaskRepository repository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
---
+++ apps/backend/src/main/java/com/tus2026/backend/Repository/TaskRepository.java
@@ -0,0 +1,16 @@
+package com.tus2026.backend.Repository;
+
+import com.tus2026.backend.Models.Task;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
+
+public interface TaskRepository extends MongoRepository<Task, String> {
+
+    @Query("{taskName:'?0'}")
+    Task findByName(String taskName);
+
+    @Query(value="{ '_id' : '?0' }", delete=true)
+    void deleteById(int id);
+}
