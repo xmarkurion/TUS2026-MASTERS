a4e32ffff1a0bf1261cb51caa17da25e9cf314e7
xmarkurion <xmarkurion@gmail.com>
2026-02-06 12:01:55 +0000

    added pretier and linting fixes

---
+++ prettier.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  printWidth: 100,
+  arrowParens: 'avoid',
+  plugins: ['prettier-plugin-tailwindcss'],
+}
