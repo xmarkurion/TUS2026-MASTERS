aa1595ba671ada7a80469b3a9b0a7677b48338c8
xmarkurion <xmarkurion@gmail.com>
2026-02-06 11:03:09 +0000

    storybook fix & up to v10

--- apps/frontend/.storybook/main.ts
+++ apps/frontend/.storybook/main.ts
@@ -1,18 +1,36 @@
+import { fileURLToPath } from "node:url";
+import { dirname } from "node:path";
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-interactions'],
+
+  core: {
+    disableTelemetry: true, // 👈 Disables telemetry
+  },
+
+  addons: [getAbsolutePath("@storybook/addon-docs")],
+
   framework: {
-    name: '@storybook/react-vite',
+    name: getAbsolutePath("@storybook/react-vite"),
     options: {},
   },
-  docs: {
-    autodocs: 'tag',
-  },
+
   async viteFinal(config) {
+    const stripStorybookSourcemapPlugin = {
+      name: 'strip-storybook-sourcemap',
+      enforce: 'post' as const,
+      transform(code: string, id: string) {
+        if (id.startsWith('/virtual:/@storybook')) {
+          return code.replace(/\/\/# sourceMappingURL=.*$/gm, '').replace(/\/*# sourceMappingURL=.*\*\//gm, '');
+        }
+        return null;
+      },
+    };
+
     return {
       ...config,
+      plugins: [...(config.plugins || []), stripStorybookSourcemapPlugin],
       build: {
         ...config.build,
         sourcemap: false,
@@ -21,8 +39,19 @@ const config: StorybookConfig = {
         ...config.esbuild,
         sourcemap: false,
       },
-    };
-  },
+      optimizeDeps: {
+        ...(config.optimizeDeps || {}),
+        esbuildOptions: {
+          ...((config.optimizeDeps && (config.optimizeDeps as any).esbuildOptions) || {}),
+          sourcemap: false,
+        },
+      },
+    } as typeof config;
+  }
 };
 
 export default config;
+
+function getAbsolutePath(value: string): any {
+  return dirname(fileURLToPath(import.meta.resolve(`${value}/package.json`)));
+}
