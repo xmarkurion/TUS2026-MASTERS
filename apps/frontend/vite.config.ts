b9d1cca42f3899959b6af79710c74ca0aef0e57a
abhijith-zero <abhijithrajesh18@gmail.com>
2026-02-08 16:04:53 +0000

    Config issues

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -2,6 +2,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 export default defineConfig({
   root: __dirname,
@@ -17,7 +19,12 @@ export default defineConfig({
     host: 'localhost',
   },
 
-  plugins: [react(), nxViteTsPaths()],
+  plugins: [react(), nxViteTsPaths(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 
   build: {
     outDir: '../../dist/apps/frontend',
