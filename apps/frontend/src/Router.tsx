01be22a1bf79038896877893f2b024ef20681c35
xmarkurion <xmarkurion@gmail.com>
2026-02-26 09:30:42 +0000

    route

--- apps/frontend/src/Router.tsx
+++ apps/frontend/src/Router.tsx
@@ -4,13 +4,15 @@ import NotMatch from './pages/NotMatch';
 import Sample from './pages/Sample';
 import ComingSoon from './pages/ComingSoon';
 import TaskBoard from './pages/TaskBoard';
+import StartupPage from './pages/StartupPage';
 
 export default function Router() {
   return (
     <Routes>
       <Route element={<AppLayout />}>
-        <Route path="" element={<TaskBoard />} />
+        <Route path="" element={<StartupPage />} />
         <Route path="pages">
+          <Route path="taskboard" element={<TaskBoard />} />
           <Route path="sample" element={<Sample />} />
           <Route path="feature" element={<ComingSoon />} />
           <Route path="taskboard" element={<TaskBoard />} />
